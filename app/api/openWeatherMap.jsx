258190afffa9d5a42b530ec0030b563e29ae4692
Wanderson Diego <wdiegomt@gmail.com>
2016-12-18 09:24:57 -0200

    Initial  commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,26 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=3c07bbbb63baae12f6e015abb48af5fa&units=metric';
+
+module.exports = {
+  getTemp: function (location) {
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+    return axios.get(requestUrl)
+      .then(function (res) {
+        if (res.data.cod && res.data.message) {
+          throw new Error(res.data.message);
+        } else {
+          return res.data.main.temp;
+        }
+      })
+      .catch(function (res) {
+        throw new Error(res.response.data.message);
+      });
+  }
+};
+
+
+// 3c07bbbb63baae12f6e015abb48af5fa
+// http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=3c07bbbb63baae12f6e015abb48af5fa&units=metric
---
+++ app/components/Examples.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var Examples = () => {
+  return (
+      <h3>Examples Component</h3>
+    )
+};
+
+module.exports = Examples;
